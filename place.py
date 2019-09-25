513d3544c2fcbc77662ad7d40a13b467352d799b
KSN99 <53466034+KSN99@users.noreply.github.com>
2019-09-25 21:13:33 +0800

    Add files via upload

--- place.py
+++ place.py
@@ -1,25 +1,15 @@
-# Create your Place class in this file
-
 
 class Place:
-    """Simple class with the required attributes for a place and the methods"""
-    pass
-    def __init__(self, name, country, priority, status):
-        """The __init__() function is called automatically every time the class is being used to create a new object."""
-        self.name=name
-        self.country=country
-        self.priority=priority
-        self.status=status
+    """Place class for represent place objects."""
+    def __init__(self, title, country, priority, status):
+        """initialize each attributes in places list"""
+        #This simple class attributes for a place (name, country, priority, visited status).
+        self.title = title
+        self.country = country
+        self.priority = priority
+        self.status = status
 
-    def __str__(self):
-        """Two methods to mark the place as unvisited and visited"""
-        if self.status == "v":
-            return "Unvisited place {} in {} ({})".format(self.name, self.country, self.status)
-        else:
-            return "Visited place {} in {} ({})".format(self.name, self.country, self.status)
-
-    def m_visited(self, *args):
-        """visited place"""
-        if self.status == "v":
-            self.status == "n"
-            return self.status
+    def mark_place(self, status):
+        """Mark as visited for places"""
+        #marking place as visited or not
+        self.status = status
